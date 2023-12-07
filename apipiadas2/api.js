49f28d20d35e649896f07749a3327e285383b68e
BluePen7 <127451613+BluePen7@users.noreply.github.com>
2023-12-06 21:54:13 -0300

    Update api.js

--- apipiadas2/api.js
+++ apipiadas2/api.js
@@ -33,7 +33,7 @@ function getNRandomJokes(n) {
     return piadasSelecionadas;
 }
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
